b0584d80c52e7845b4a1b13902f814feb9dc192c
aliaksei_kulik <aliaksei_kulik@witns.dev>
2025-01-06 18:07:56 +0100

    update a release

--- Formula/aws-sso-login.rb
+++ Formula/aws-sso-login.rb
@@ -2,7 +2,7 @@ class AwsSsoLogin < Formula
   desc "CLI that streamlines AWS SSO authentication and credentials management"
   homepage "https://github.com/witnsby/aws-sso-login"
   url "https://github.com/witnsby/aws-sso-login/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "4f2b621e2eaf5649dd5ea87cb2516868a64e1fe854d92ed7c8dd4b02f924be6f"
+  sha256 "b37b80a109d823b112242ec65befe951df258829990dbec9394005262f4f680f"
   license "Apache-2.0"
 
   depends_on "go" => :build
